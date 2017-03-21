3222acc72f4dee2f5632d7417fc440d609b9014b
Francesco D'Argento <francescodargento@MacBook-Pro-di-Francesco.local>
2017-03-21 11:08:46 +0100

    First Java script file

---
+++ js/javascript.js
@@ -0,0 +1,25 @@
+let name =  "Francesco";
+
+let numKids = 0;
+
+let address = "Vigerslevvej 334";
+
+let hair = "Brown";
+
+let partnerName = "Gabriele";
+
+let jobtytle="Student";
+
+numKids = 4;
+
+let age=30;
+
+let info=name+"live at"+address;
+console.log(age);
+age = age+1;
+console.log(address, name, age);
+
+console.log("You will be " + name + " in " + address + "he has" + numKids + "kids, with his partner" + partnerName);
+
+/* use this for adding comment */
+
